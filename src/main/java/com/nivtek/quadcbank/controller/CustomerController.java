c3b7309e2ffefd17cbfc44d7c38ca7fa20f86b0a
Asim <asim.232@gmail.com>
2020-04-01 10:00:01 -0500

    Documented Customer Controller Class

--- src/main/java/com/nivtek/quadcbank/controller/CustomerController.java
+++ src/main/java/com/nivtek/quadcbank/controller/CustomerController.java
@@ -16,6 +16,9 @@ import com.nivtek.quadcbank.entity.Customer;
 import com.nivtek.quadcbank.service.CustomerService;
 
 /**
+ * CustomerController class basically responsible to handle the login and
+ * displaying dashboard if loggedin or redirect to login if unable to verify.
+ * 
  * @author AsimSubedi
  *
  */
@@ -26,17 +29,26 @@ public class CustomerController {
 	private CustomerService customerService;
 
 	/**
-	 * This is login page. Always redirect users to login page at beginning
+	 * This method displays the Login Page.
 	 * 
 	 * @return login page
 	 */
 	@RequestMapping(value = { "/", "/login" })
-	public String login() {
+	public String displayLoginPage() {
 
 		return "login";
 
 	}
 
+	/**
+	 * This method handles the user login credentials. If user is present, show him
+	 * dashboard, else redirect to login page.
+	 * 
+	 * @param request
+	 * @param model
+	 * @param session
+	 * @return user-dashboard page if login successful
+	 */
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
 	public String handleCustomerLogin(HttpServletRequest request, Model model, HttpSession session) {
 
@@ -58,24 +70,39 @@ public class CustomerController {
 		return viewName;
 
 	}
-	
-	@RequestMapping(value="/user-dashboard")
-	public String userDashboard(HttpSession session) {
-		
-		if(session.getAttribute("customer") != null)
+
+	/**
+	 * This method is responsible to show the user dashboard view. Only the loggedIn
+	 * user can view this page.
+	 * 
+	 * @param session
+	 * @return user-dashboard view
+	 */
+	@RequestMapping(value = "/user-dashboard")
+	public String showUserDashboard(HttpSession session) {
+
+		if (session.getAttribute("customer") != null)
 			return "user-dashboard";
-		
+
 		else
 			return "redirect:login";
-		
+
 	}
-	
-    @RequestMapping(value = "/logout")
-    public String logout(HttpServletRequest request, HttpSession session) {
-        session.invalidate();
-        session = request.getSession(true);
-        return "redirect:login?action=logout";
-        
-    }
+
+	/**
+	 * This method is responsible to logout the user by invalidating the sessions.
+	 * After logout user is redirected to login page.
+	 * 
+	 * @param request
+	 * @param session
+	 * @return login-page view
+	 */
+	@RequestMapping(value = "/logout")
+	public String logout(HttpServletRequest request, HttpSession session) {
+		session.invalidate();
+		session = request.getSession(true);
+		return "redirect:login?action=logout";
+
+	}
 
 }
---
+++ src/test/java/com/nivtek/quadcbank/controller/CustomerControllerTest.java
@@ -0,0 +1,34 @@
+package com.nivtek.quadcbank.controller;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class CustomerControllerTest {
+
+	@Test
+	@Ignore("Ignored for Now")
+	public void testLogin() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	@Ignore("Ignored for Now")
+	public void testHandleCustomerLogin() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	@Ignore("Ignored for Now")
+	public void testUserDashboard() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	@Ignore("Ignored for Now")
+	public void testLogout() {
+		fail("Not yet implemented");
+	}
+
+}
